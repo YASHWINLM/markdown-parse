3eaef67bb619e2bc99993ec265b88ff7c4f3c138
YASHWINLM <sashimiym@gmail.com>
2022-02-24 18:07:52 -0800

    added snippet 2 tester

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,32 +7,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
+    // @Test
+    // public void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+
+    // @Test
+    // public void additionbad() {
+    //     assertEquals(2, 1 + 1);
+    // }
+
+    // @Test
+    // public void testingGenericFile() throws IOException{
+    //     Path fileName = Path.of("test-file.md");
+    //     assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(Files.readString(fileName)));
+    // }
+
+    // @Test 
+    // public void testingEmpty()throws IOException{
+    //     Path fileName=Path.of("test-empty.md");
+    //     System.out.println(fileName);
+    //     assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(fileName)));
+    // }
+
+    // public void testingEmpty2()throws IOException{
+    //     Path fileName=Path.of("test-empty.md");
+    //     System.out.println(fileName);
+    //     assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(fileName)));
+    // }
 
     @Test
-    public void additionbad() {
-        assertEquals(2, 1 + 1);
+    public void testingSnippet1()throws IOException{
+        Path fileName=Path.of("snippet-1.md");
+        assertEquals(List.of("`google.com", "google.com", "ucsd.edu"), MarkdownParse.getLinks(Files.readString(fileName)));
+        
     }
 
     @Test
-    public void testingGenericFile() throws IOException{
-        Path fileName = Path.of("test-file.md");
-        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(Files.readString(fileName)));
+    public void testingSnippet2()throws IOException{
+        Path fileName=Path.of("snippet-2.md");
+        assertEquals(List.of("a.com", "a.com(())", "example.com"), MarkdownParse.getLinks(Files.readString(fileName)));
+        
     }
 
-    @Test 
-    public void testingEmpty()throws IOException{
-        Path fileName=Path.of("test-empty.md");
-        System.out.println(fileName);
-        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(fileName)));
-    }
-
-    public void testingEmpty2()throws IOException{
-        Path fileName=Path.of("test-empty.md");
-        System.out.println(fileName);
-        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(fileName)));
-    }
 }
